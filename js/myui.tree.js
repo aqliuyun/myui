02524a000b2382c7c8b098ee8f17d4d580808b62
aqliuyun <326058151@qq.com>
2015-07-08 10:53:08 +0800

    完善树型控件
    完善树型控件

--- js/myui.tree.js
+++ js/myui.tree.js
@@ -4,19 +4,30 @@
 		modelKey:'key',
 		modelText :'text',
 		modelDescription :'description',
+		modelAsyncLoad:'asyncload',
 		modelIcon: '',
 		modelParent :'parent',
 		modelChildren:'children',
-		dataparser:'tree'
+		dataparser:'tree',
+		selectmode:'single',
 	};
 
 	var Tree = libs.Class.define({
-		__constructor:function(options){
+		__constructor:function(options){			
 			this.id = libs.Utils.unique();
-			this.options = options;
+			this.options = $.extend({},default_options,options);
+			this.selectkey = null;
+			this.selectkeys = [];
 			this.events = this.options.events || {};
 		},
-		events:{},
+		events:{
+			onselet:null,
+			ontreecomplete:null,
+			onasyncload:null
+		},
+		dom:function () {
+			return $('#myui_tree_'+this.id);
+		},
 		__build:function(){
 			var that = this;
 			var options = this.options;
@@ -28,8 +39,9 @@
 			else if(options.dataparser == 'tree')
 			{
 				codes = that.__parsedata_tree();
-			}					
-			this.container.append(codes.join(''));
+			}
+			this.container.append('<div id="myui_tree_'+this.id + '">'+codes.join('')+'</div>');
+			this.events.ontreecomplete && this.events.ontreecomplete();
 		},
 		__parsedata_list:function(){
 			var options = this.options;
@@ -43,12 +55,18 @@
 				var modelText = options.modelText ? data[options.modelText] : '';
 				var description = options.modelDescription ? data[options.modelDescription] : '';				
 				var modelIcon = options.modelIcon ? data[options.modelIcon] : '';
+				var asyncload = options.modelAsyncLoad ? data[options.modelAsyncLoad] : false;
 				if(modelParent)
 				{
 					continue;
 				}
 				var childrencodes = this.__parsedata_list_children(modelKey);
-				codes.push(strFormat('<div class="tree-item {0}" data-children="{1}">',childrencodes.length > 0 ? 'tree-parent-item':'',childrencodes.length > 0));
+				codes.push(strFormat('<div class="tree-item {0}" data-children="{1}" data-id="{2}">',
+					childrencodes.length > 0 ? 'tree-parent-item':'',
+					childrencodes.length > 0,
+					modelKey,
+					asyncload ? 'tree-parent-item tree-asyncload-item closed' : ''
+					));
 				codes.push(strFormat('<i class="icon {0}"></i>', modelIcon || ''));
 				codes.push('<div class="text">');
 				codes.push(strFormat('<div class="head-text">{0}</div>',modelText));
@@ -72,6 +90,7 @@
 				var modelText = options.modelText ? data[options.modelText] : '';
 				var description = options.modelDescription ? data[options.modelDescription] : '';
 				var modelIcon = options.modelIcon ? data[options.modelIcon] : '';
+				var asyncload = options.modelAsyncLoad ? data[options.modelAsyncLoad] : false;
 				if(modelParent != parent) { continue; }
 				if(!begin){
 					codes.push('<div class="tree-list">');
@@ -79,7 +98,12 @@
 					end = true;
 				}
 				var childrencodes = this.__parsedata_list_children(modelKey);
-				codes.push(strFormat('<div class="tree-item {0}" data-children="{1}">',childrencodes.length > 0 ? 'tree-parent-item':'',childrencodes.length > 0));
+				codes.push(strFormat('<div class="tree-item {0} {3}" data-children="{1}" data-id="{2}">',
+					childrencodes.length > 0 ? 'tree-parent-item':'',
+					childrencodes.length > 0,
+					modelKey,
+					asyncload ? 'tree-parent-item tree-asyncload-item closed' : ''
+					));
 				codes.push(strFormat('<i class="icon {0}"></i>', modelIcon || ''));
 				codes.push('<div class="text">');
 				codes.push(strFormat('<div class="head-text">{0}</div>',modelText));
@@ -106,8 +130,14 @@
 				var description = options.modelDescription ? data[options.modelDescription] : '';				
 				var modelIcon = options.modelIcon ? data[options.modelIcon] : '';
 				var modelChildren = options.modelChildren ? data[options.modelChildren] : [];
+				var asyncload = options.modelAsyncLoad ? data[options.modelAsyncLoad] : false;
 				codes.push('<div class="tree-list">');
-				codes.push(strFormat('<div class="tree-item {0}" data-children="{1}">',modelChildren.length > 0 ?'tree-parent-item':'',modelChildren.length > 0));
+				codes.push(strFormat('<div class="tree-item {0}" data-children="{1}" data-id="{2}">',
+					modelChildren.length > 0 ?'tree-parent-item':''
+					,modelChildren.length > 0,
+					modelKey,
+					asyncload ? 'tree-parent-item tree-asyncload-item closed' : ''
+					));
 				codes.push(strFormat('<i class="icon {0}"></i>', modelIcon || ''));
 				codes.push('<div class="text">');
 				codes.push(strFormat('<div class="head-text">{0}</div>',modelText));
@@ -147,14 +177,198 @@
                     }
 				});
 			}			
-		}	,	
+		},			
+		find:function(modelKey){
+			if(this.options.dataparser == 'list')
+			{
+				return this.__finddata_list(modelKey);
+			}
+			else if(this.options.dataparser == 'tree')
+			{
+				return this.__findata_tree(modelKey);
+			}
+			return null;
+		},
+		__finddata_list:function(modelKey){
+			var datas = this.options.datas;
+			for (var i = 0; i < datas.length; i++) {
+				var data = datas[i];
+				if(data[this.options.modelKey] == modelKey){
+					return data;
+				}
+			}
+			return null;
+		},
+		__findata_tree:function(modelKey,children){
+			var datas = children && children.length > 0 ? children : this.options.datas;
+			for (var i = 0; i < datas.length; i++) {
+				var data = datas[i];
+				if(data[this.options.modelKey] == modelKey){
+					return data;
+				}
+				if(data[this.options.modelChildren] && data[this.options.modelChildren].length > 0)
+				{
+					return this.__findata_tree(modelKey,data[this.options.modelChildren]);
+				}
+			}
+			return null;
+		},
+		__async_load:function(modelKey,onasyncloadend){
+			var that =this;
+			var options = that.options;
+			var $dom = that.dom();
+			var params = {};
+			params[options.modelParent] = modelKey;
+			params = $.extend({}, options.data,params);
+			if(options.url)
+			{
+				$.ajax({
+					url:options.url,
+					data:params,
+                    dataType:options.dataType,
+                    success:function(result){                    	
+						if(result.dataparser == 'list')
+						{
+							options.datas = options.datas.concat(result.datas);
+							var data = that.find(modelKey);
+							var selector = '.tree-item[data-id='+modelKey+'] .text';
+							var codes = that.__parsedata_list_children(modelKey);
+							$(selector,$dom).append(codes.join(''));
+							onasyncloadend && onasyncloadend(result.datas != null);
+						}
+						else
+						{
+							var data = that.find(modelKey);
+							data[that.modelChildren] = result.datas;
+							var selector = '.tree-item[data-id='+modelKey+'] .text';
+							var codes = that.__parsedata_tree(result.datas);
+							$(selector,$dom).append(codes.join(''));
+							onasyncloadend && onasyncloadend(result.datas != null);
+						}                    	
+                    }
+				});
+			}
+			else if(this.events.onasyncload)
+			{
+				this.events.onasyncload(params,function(result){
+					if(result.dataparser == 'list')
+					{
+						options.datas = options.datas.concat(result.datas);
+						var data = that.find(modelKey);
+						var selector = '.tree-item[data-id='+modelKey+'] .text';
+						var codes = that.__parsedata_list_children(modelKey);
+						$(selector,$dom).append(codes.join(''));			
+						onasyncloadend && onasyncloadend(result.datas != null);
+					}
+					else
+					{
+						var data = that.find(modelKey);
+						data[that.modelChildren] = result.datas;
+						var selector = '.tree-item[data-id='+modelKey+'] .text';
+						var codes = that.__parsedata_tree(result.datas);
+						$(selector,$dom).append(codes.join(''));	
+						onasyncloadend && onasyncloadend(result.datas != null);			
+					}
+				});
+			}
+		},
+		select:function(modelKey){
+			var that = this;
+			var options = that.options;
+			var $dom = that.dom();
+			that.events.onselect && that.events.onselect(modelKey);			
+			if(options.selectmode == 'single')
+			{
+				that.selectkey = modelKey;
+				$('.tree-item .head-text',$dom).removeClass('highlight');
+				var selector = '[data-id='+modelKey+']>.text>.head-text';
+				$(selector,$dom).addClass('highlight');
+			}
+			else
+			{
+				if(libs.Utils.arrayContains(that.selectkeys,modelKey)){ return; }
+				that.selectkeys.push(modelKey);
+				var selector = '[data-id='+modelKey+']>.text>.head-text';
+				$(selector,$dom).addClass('highlight');
+			}
+		},
+		isselect:function(modelKey){
+			var that = this;
+			var options = that.options;
+			if(options.selectmode == 'single')
+			{
+				return (that.selectkey == modelKey);
+			}
+			else
+			{
+				return libs.Utils.arrayContains(that.selectkeys,modelKey);
+			}
+			return false;
+		},
+		unselect:function(modelKey){
+			var that = this;
+			var options = that.options;
+			var $dom = that.dom();
+			if(options.selectmode == 'single')
+			{
+				that.selectkey = null;
+				var selector = '[data-id='+modelKey+']>.text>.head-text';
+				$(selector,$dom).removeClass('highlight');
+			}
+			else
+			{
+				libs.Utils.arrayRemove(that.selectkeys,modelKey);
+				var selector = '[data-id='+modelKey+']>.text>.head-text';
+				$(selector,$dom).removeClass('highlight');
+			}
+		},
+		getSelectData:function(){
+			return this.find(this.selectkey);
+		},
+		getSelectDatas:function(){
+			var result = [];
+			for (var i = 0; i < this.selectkeys.length; i++) {
+				var key = this.selectkeys[i]
+				result.push(this.find(key));
+			};
+			return result;
+		},
 		bindEvents:function(){
-			$('.tree-parent-item>.icon').off('click.myui-tree').on('click.myui-tree',function(){
-				console.log('click');
-				$(this).parent().toggleClass('closed');
+			var that = this;
+			var options = that.options;
+			var $dom = that.dom();
+			$('.tree-parent-item>.icon',$dom).off('click.myui-tree').on('click.myui-tree',function(){
+				$(this).closest('.tree-parent-item').toggleClass('closed');
+			});
+			$('.tree-asyncload-item>.icon',$dom).off('click.myui-tree.asyncload').on('click.myui-tree.asyncload',function(){
+				var $this = $(this);
+				var $treeitem = $(this).closest('.tree-item');
+				var modelKey = $treeitem.attr('data-id');
+				that.__async_load(modelKey,function(haschildren){
+					$this.off('click.myui-tree.asyncload');
+					$treeitem.attr('data-children',haschildren);
+				});
+			});
+			$('.tree-item .head-text',$dom).off('click.myui-tree').on('click.myui-tree',function(){
+				var $this = $(this);
+				var $treeitem = $this.closest('.tree-item');
+				var modelKey = $treeitem.attr('data-id');
+				if(that.isselect(modelKey))
+				{
+					that.unselect(modelKey);
+				}
+				else
+				{
+					that.select(modelKey);
+				}
 			});
 		},
-		unBindEvents:function(){}
+		unBindEvents:function(){
+			var that = this;
+			var $dom = that.dom();
+			$('.tree-parent-item>.icon',$dom).off('click.myui-tree');
+			$('.tree-item .head-text',$dom).off('click.myui-tree');
+		}
 	});
 	libs.Widgets.Tree = Tree;
 })(myui,jQuery);
